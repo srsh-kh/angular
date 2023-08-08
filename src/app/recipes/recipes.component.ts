912e6acafaf66311e2c439a45c98d88587510a49
Soroush Khosh <sur.khosh@outlook.com>
2023-08-08 10:12:51 +0330

    x

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from "../recipe.model";
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://www.add1tbsp.com/wp-content/uploads/2021/10/20210926-Epic-BoneBroth-TortillaSoup3650-1024x1536.jpg")
+  ];
 
   constructor() { }
 
