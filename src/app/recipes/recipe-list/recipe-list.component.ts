528794432918ff3d6482cac6c8655fd092643ae2
Soroush Khosh <sur.khosh@outlook.com>
2023-08-09 20:58:03 +0330

    x

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://www.add1tbsp.com/wp-content/uploads/2021/10/20210926-Epic-BoneBroth-TortillaSoup3650-1024x1536.jpg"),
+    new Recipe("A Test Recipe", "This is simply a test", "https://www.add1tbsp.com/wp-content/uploads/2021/10/20210926-Epic-BoneBroth-TortillaSoup3650-1024x1536.jpg")
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
