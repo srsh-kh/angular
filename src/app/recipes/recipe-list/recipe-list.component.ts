be79f8b62b1dff26b4c0dad58286807308326b61
Soroush Khosh <sur.khosh@outlook.com>
2023-08-24 20:59:37 +0330

    ط

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from "../recipe.model";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a test", "https://www.add1tbsp.com/wp-content/uploads/2021/10/20210926-Epic-BoneBroth-TortillaSoup3650-1024x1536.jpg"),
-    new Recipe("A Test Recipe", "This is simply a test", "https://www.add1tbsp.com/wp-content/uploads/2021/10/20210926-Epic-BoneBroth-TortillaSoup3650-1024x1536.jpg")
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.add1tbsp.com/wp-content/uploads/2021/10/20210926-Epic-BoneBroth-TortillaSoup3650-1024x1536.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.add1tbsp.com/wp-content/uploads/2021/10/20210926-Epic-BoneBroth-TortillaSoup3650-1024x1536.jpg'
+    ),
   ];
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
