3325c117f87eedc9f4857845501f9bf93086fa2f
Soroush Khosh <sur.khosh@outlook.com>
2023-11-04 12:45:29 +0330

    b4

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -50,7 +50,7 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onDeleteIngredient(index: number) {
-    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    (<FormArray>this.recipeForm.get('ingredients')).clear();
   }
 
   onCancel() {
