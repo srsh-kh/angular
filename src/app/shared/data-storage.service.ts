4aa7f3728e94c15d645e29aa8ff25fbd7774e0a8
Soroush Khosh <sur.khosh@outlook.com>
2023-11-27 16:59:33 +0330

    x

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 
@@ -14,6 +15,12 @@ export class DataStorageService {
         )
         .subscribe(response => {
             console.log(response);
+        })
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>("https://ng-course-recipe-book-5de81-default-rtdb.firebaseio.com/recipes.json").subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
         })
     }
 }
