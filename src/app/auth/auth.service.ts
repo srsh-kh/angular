d29d9ceeaad7e8680a7fa6af0716de36133755dc
Soroush Khosh <sur.khosh@outlook.com>
2023-12-01 22:51:15 +0330

    x

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+@Injectable({ providedIn: 'root'})
+export class AppService {
+    constructor(private http: HttpClient) {}
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBx_At05dFDh2KupET07i6po7ZeCQc29Vw",
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        })
+    }
+}
