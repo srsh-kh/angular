5e0df13fa019d0372e2679d0b97c8b77e74262ee
Soroush Khosh <sur.khosh@outlook.com>
2023-12-14 14:15:37 +0330

    x

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBx_At05dFDh2KupET07i6po7ZeCQc29Vw',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBx_At05dFDh2KupET07i6po7ZeCQc29Vw',
         {
           email: email,
           password: password,
@@ -49,7 +49,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBx_At05dFDh2KupET07i6po7ZeCQc29Vw',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBx_At05dFDh2KupET07i6po7ZeCQc29Vw',
         {
           email: email,
           password: password,
