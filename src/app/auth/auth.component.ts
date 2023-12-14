28ef43b3ae0eb3228b4173617d00477c0356edc7
Soroush Khosh <sur.khosh@outlook.com>
2023-12-14 13:58:16 +0330

    x

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { AuthService, AuthResponseData } from './auth.service';
 import { AlertComponent } from '../shared/alert/alert.component';
@@ -11,11 +11,13 @@ import { PlaceholderDirective } from '../shared/placeholder/placeholder.directiv
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy{
   isLoginMode = true;
   isLoading = false;
   error: string = null;
   @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+
+  private closeSub: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -65,11 +67,24 @@ export class AuthComponent {
     this.error = null;
   }
 
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
+
   private showErrorAlert(message: string) {
     const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
   };
   const hostViewContainerRef = this.alertHost.viewContainerRef;
   hostViewContainerRef.clear();
 
-  hostViewContainerRef.createComponent(alertCmpFactory);
+  const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+
+  componentRef.instance.message = message;
+  this.closeSub = componentRef.instance.close.subscribe(() => {
+    this.closeSub.unsubscribe();
+    hostViewContainerRef.clear();
+  });
+}
 }
