0395de3fe2c130a957bcea83282e49f32cf4f388
Soroush Khosh <sur.khosh@outlook.com>
2023-12-27 13:00:36 +0330

    x

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyBx_At05dFDh2KupET07i6po7ZeCQc29Vw'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyBx_At05dFDh2KupET07i6po7ZeCQc29Vw'
 };
 
 /*
